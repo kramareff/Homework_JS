76e008efb82460cfbfd5355f87072a6e3022698c
kramareff <kramareff.vlad@yandex.by>
2025-08-02 15:12:01 +0300

    2.4

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,16 +7,19 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.floor(Math.random() * 6) // your code
+let dice2 = Math.floor(Math.random() * 6) // your code
+let isWinningDouble = false
 
-/*
+
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
+
+if((dice1>3) && (dice2>3) && (dice1=dice2)) {
+  isWinningDouble = true
+if (isWinningDouble = true) {
   console.log('Выигрышный дубль!')
 } else {
   console.log('Не выигрышный дубль.')
 }
-*/
+}
