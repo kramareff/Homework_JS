15ee3ad34c2a55878a80b5833c06adb9e844ec1b
kramareff <kramareff.vlad@yandex.by>
2025-08-03 12:34:03 +0300

    3.3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,5 +1,5 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
 function calculateAverage(a, b) {
-   console.log((a+bs)/2);
+   return ((a+bs)/2);
 }
