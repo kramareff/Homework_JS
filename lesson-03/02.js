f07217275c92bd5bca5cbab3d4dccfc0c8a7d5a2
kramareff <kramareff.vlad@yandex.by>
2025-08-03 12:42:39 +0300

    3.1

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,11 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+function isEven(a) {
+    return 
+    if((a%2) == 0) {
+        isEven = true
+    } else {
+        isEven = false
+    }
+}
