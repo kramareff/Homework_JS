61dceb32f4bd0e8c8775e7e6ac83b42446c507b6
kramareff <kramareff.vlad@yandex.by>
2025-08-07 22:25:37 +0300

    4.4

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,16 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+    let str2 =" "
+    for(i=0;i < str.length; i++) {
+    str2+=str[i]+str[i]
+    }
+return str2
+
+}
+str='hel'
+console.log(doubleEachCharacter('hello!'))
+// console.log(str[1]='O',str)
+// let a = 'aa'.split('').forEach(item=>item+item)
+// console.log('hello'.split('').map(item=>item+item).join(''))
