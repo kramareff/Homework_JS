86f7cc97aa923c0f35c70f2f130c33180c7beafc
kramareff <kramareff.vlad@yandex.by>
2025-08-07 21:37:35 +0300

    4.1

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -14,5 +14,32 @@
 
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
+// function includesElement(array, element) {
+//    let flag =false
+//     for(let i = 0; i < array.length; i++)
+//     {
+//         if (array[i] === element) {
+//             flag= true
+//         }  
+//     }
+//     return flag
+// }
+// 
 
-function findUniqueElements() {}
+function findUniqueElements(array) {
+    let arrayNew = []
+    for(let i = 0; i < array.length; i++) 
+    {
+        
+        if (arrayNew.indexOf(array[i])>=0){
+        
+        
+     }else{arrayNew.push(array[i])}
+        
+     
+    }
+    return arrayNew
+}
+console.log(findUniqueElements([1, 2, 3, 2, 1, 4,4,6,8,5,5]))
+
+// console.log(includesElement([1, 2, 3], 3))
